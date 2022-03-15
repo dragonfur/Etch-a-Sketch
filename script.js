1dd2d33bc959f334bd07e07a03a32930b5851dae
Jemuel <f2pskilleruel@gmail.com>
2022-03-15 10:49:44 +0300

    Add clear and erase functions
    Added clear and erase function capabilities

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 //script
 
+var defaultColor = "black"
+
 const container = document.querySelector(".container")
+const clear = document.createElement("button")
+const resize = document.createElement("button")
+const erase = document.createElement("button")
+clear.textContent = "Clear"
+resize.textContent = "Resize"
+erase.textContent = "Erase"
+
+const tds = document.querySelectorAll('.square');
+tds.forEach((square) => {
+  square.style.setProperty('--td-background-color', '#00ff00');
+});
+
 let move = false
-var size = 16
+var size = 64
 makeSquares(size)
 
 document.addEventListener("mousedown", () => {
@@ -19,6 +33,7 @@ function boardSize() {
 }
 
 function makeSquares(size){
+    
     for (let i = 0; i < (size * size); i++){
         const square = document.createElement("div")
         square.classList.add("square")
@@ -31,11 +46,29 @@ function makeSquares(size){
                 startDrawing(e)
             }
         })
+        menuButtons()
+        
+        //clear action
+        clear.addEventListener("click", () => {
+            square.style.backgroundColor = "white"
+        })
     }
 }
 
 function startDrawing(e) {
-    e.target.style.backgroundColor = "blue"
+    e.target.style.backgroundColor = defaultColor
 }
 
+function menuButtons(){
+    document.body.appendChild(erase).classList.add("menu")
+    document.body.appendChild(resize).classList.add("menu")
+    document.body.appendChild(clear).classList.add("menu")
+}
 
+//resize and erase actions
+resize.addEventListener("click", () => {
+    size = prompt("Pick a number")
+    const squares = document.getElementsByClassName("square")
+    squares.container.remove(squares)
+    makeSquares(size)
+})
