22b62d0475868b63a4ce0c56f98e4cc25cd81226
Jemuel <f2pskilleruel@gmail.com>
2022-03-15 16:48:15 +0300

    Add erase and rainbow functions
    Added erase and rainbow functions to complete whole functionality
    of the website.

--- script.js
+++ script.js
@@ -7,9 +7,19 @@ let sizeIsNum = true
 const container = document.querySelector(".container")
 const clear = document.querySelector(".clear")
 const resize = document.querySelector(".resize")
+const erase = document.querySelector(".erase")
+const rainbow = document.querySelector(".rainbow")
 
+
+var randomR
+var randomG
+var randomB
 
 let move = false
+let erasing = false
+let rainbowing = false
+let isColor = true
+var color = defaultColor
 var size = defaultSize
 var width = 500/size
 makeSquares(size)
@@ -31,38 +41,87 @@ function makeSquares(size){
         square.classList.add("square")
         container.appendChild(square)
         square.addEventListener("click", function (e) {
-            startDrawing(e)
+            if (erasing === false){
+            startDrawing(e, defaultColor)
+            }
+            else if (erasing === false && rainbowing === true){
+                rainbowDraw()
+                startDrawing(e, "rgb(" + randomR +", " + randomG + ", " + randomB +")")
+            }
+            else
+            startDrawing(e, "white")
         })
         square.addEventListener("mouseenter", function (e) {
             if (move === true){
-                startDrawing(e)
+                if (erasing === false && rainbowing === false){
+                startDrawing(e, defaultColor)
+                }
+                else if (erasing === false && rainbowing === true){
+                    rainbowDraw()
+                    startDrawing(e, "rgb(" + randomR +", " + randomG + ", " + randomB +")")
+                }
+                else
+                startDrawing(e, "white")
             }
         })
+
         square.style.width = width + "px"
-        square.style.height = width + "px"
-        
+        square.style.height = width + "px"  
     }
+}
+
+function rainbowDraw() {
+    randomR = Math.floor(Math.random() * 256)
+    randomG = Math.floor(Math.random() * 256)
+    randomB = Math.floor(Math.random() * 256)
+    return randomR, randomG, randomB
+}
+
+erase.addEventListener("click", () => {
+    erasing = !erasing
+})
+
+rainbow.addEventListener("click", () => {
+    rainbowing = !rainbowing
+    isColor = !isColor
+    if (isColor === true){
+        rainbow.textContent = "Color"
+    }
+    else if (isColor === false){
+        rainbow.textContent = "Rainbow"
+    }
+})
+
+function reset(){
+    isColor = true
+    rainbowing = false
+    erasing = false
 }
 
 clear.addEventListener("click", () => {
     container.innerHTML = " "
+    isColor = true
+    rainbowing = false
+    erasing = false
     makeSquares(size)
 })
 
 resize.addEventListener("click", () => {
-    size = parseInt(Math.floor(prompt("Pick a size from 1-100")))
+    size = parseInt(prompt("Pick a size from 1-100"))
     console.log(size)
     if (size <= 100 && size >= 1){
     container.innerHTML = " "
+    reset()
     makeSquares(size)
     return size
     }
     else 
     container.innerHTML = " "
+    reset()
     makeSquares(defaultSize)
     return defaultSize
 })
 
-function startDrawing(e) {
-    e.target.style.backgroundColor = defaultColor
+function startDrawing(e, color) {
+    e.target.style.backgroundColor = color
 }
